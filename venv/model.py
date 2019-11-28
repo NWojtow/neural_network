b05ccfc47339726fc141f5a6553cb63a834b0784
NWojtow <n.wojtowicz098@gmail.com>
2019-11-28 14:49:52 +0100

    Fix | better accuracy

--- venv/model.py
+++ venv/model.py
@@ -1,70 +1,60 @@
-import pandas as pd
+from keras.preprocessing.image import ImageDataGenerator
+import keras
+from keras.models import Sequential
+from keras.layers import Conv2D, Activation, MaxPooling2D, Flatten, Dense
+from keras.optimizers import adam
 import numpy as np
-import tensorflow as tf
-import os
-from tensorflow.keras.preprocessing import image
-from zipfile import ZipFile
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-
 
 test_dir="/home/norbert/Pulpit/dog vs cat/dataset/test_set"
 train_dir="/home/norbert/Pulpit/dog vs cat/dataset/training_set"
 
-train_dir_cats = train_dir + '/cats'
-train_dir_dogs = train_dir + '/dogs'
-test_dir_cats = test_dir + '/cats'
-test_dir_dogs = test_dir + '/dogs'
 
-print('number of cats training images - ',len(os.listdir(train_dir_cats)))
-print('number of dogs training images - ',len(os.listdir(train_dir_dogs)))
-print('number of cats testing images - ',len(os.listdir(test_dir_cats)))
-print('number of dogs testing images - ',len(os.listdir(test_dir_dogs)))
-
-data_generator = ImageDataGenerator(rescale = 1.0/255.0, zoom_range = 0.2)
+train_datagen = ImageDataGenerator(rescale = 1./255)
+test_datagen = ImageDataGenerator(rescale = 1./255)
 
 
+batch_size=64
 
-batch_size = 32
-training_data = data_generator.flow_from_directory(directory = train_dir,
-                                                   target_size = (64, 64),
-                                                   batch_size = batch_size,
-                                                   class_mode = 'binary')
-testing_data = data_generator.flow_from_directory(directory = test_dir,
-                                                  target_size = (64, 64),
-                                                  batch_size = batch_size,
-                                                  class_mode = 'binary')
+training_set = train_datagen.flow_from_directory(train_dir,
+target_size = (100, 100),
+batch_size = batch_size,
+color_mode='rgb',
+class_mode = 'binary',
+shuffle=True)
 
-model = Sequential()
-model.add(Conv2D(filters = 32, kernel_size = (3, 3), activation = 'relu', input_shape = training_data.image_shape))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(rate = 0.3))
-model.add(Conv2D(filters = 64, kernel_size = (3, 3), activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(rate = 0.2))
-model.add(Conv2D(filters = 126, kernel_size = (3, 3), activation = 'relu'))
-model.add(MaxPooling2D(pool_size = (2, 2)))
-model.add(Dropout(rate = 0.15))
-model.add(Flatten())
-model.add(Dense(units = 32, activation = 'relu'))
-model.add(Dropout(rate = 0.15))
-model.add(Dense(units = 64, activation = 'relu'))
-model.add(Dropout(rate = 0.1))
-model.add(Dense(units = len(set(training_data.classes)), activation = 'softmax'))
-model.compile(optimizer = 'adam', loss = 'sparse_categorical_crossentropy', metrics = ['accuracy'])
+test_set = test_datagen.flow_from_directory(test_dir,
+target_size = (100, 100),
+batch_size = batch_size,
+color_mode='rgb',
+class_mode = 'binary')
 
-model.summary()
+classifier = Sequential()
 
-fitted_model = model.fit_generator(training_data,
-                        steps_per_epoch = 1000,
-                        epochs = 2,
-                        validation_data = testing_data,
-                        validation_steps = 1000)
+classifier.add(Conv2D(32, (3, 3), input_shape = (100, 100, 3)))
+classifier.add(Activation("relu"))
+classifier.add(MaxPooling2D(pool_size = (3, 3)))
+classifier.add(Conv2D(64, (3, 3), input_shape = (100, 100, 3)))
+classifier.add(Activation("relu"))
+classifier.add(MaxPooling2D(pool_size = (3, 3)))
+
+classifier.add(Flatten())
+
+classifier.add(Dense(64))
+classifier.add(Activation("relu"))
+classifier.add(Dense(128))
+classifier.add(Activation("relu"))
+classifier.add(Dense(activation = 'sigmoid', units=1))
+
+classifier.compile(optimizer = 'adam', loss = 'binary_crossentropy', metrics = ['accuracy'])
+
+classifier.summary()
+
+classifier.fit_generator(training_set,
+                        steps_per_epoch=np.ceil(training_set.samples / batch_size),
+                        epochs=8,
+                        validation_steps=np.ceil(test_set.samples / batch_size),
+                         validation_data=test_set
+                        )
 
 
-
-model.save('my_model.h5')
-
-
+classifier.save('my_model.h5')
--- venv/test.py
+++ venv/test.py
@@ -1,34 +1,22 @@
-import pandas as pd
+from tensorflow.keras.preprocessing import image
 import numpy as np
 import tensorflow as tf
-import os
-from tensorflow.keras.preprocessing import image
-from zipfile import ZipFile
-from tensorflow.keras.models import Sequential
-from tensorflow.keras.layers import Conv2D, MaxPooling2D, Flatten, Dense, Dropout, Activation
-from tensorflow.keras.preprocessing.image import ImageDataGenerator
-
-test_dir="/home/norbert/Pulpit/dog vs cat/dataset/test_set"
-
-
-test_dir_cats = test_dir + '/cats'
-test_dir_dogs = test_dir + '/dogs'
-
-
 
 model = tf.keras.models.load_model('/home/norbert/PycharmProjects/neural_network/venv/my_model.h5')
 
-def testing_image(image_directory):
-    test_image = image.load_img(image_directory, target_size = (64, 64))
-    test_image = image.img_to_array(test_image)
-    test_image = np.expand_dims(test_image, axis = 0)
-    result = model.predict(x = test_image)
-    print(result)
-    if result[0][0]  == 1:
-        prediction = 'It\'s a Dog'
-    else:
-        prediction = 'It\'s a Cat'
-    return prediction
+test_dir="/home/norbert/Pulpit/dog vs cat/dataset/test_set"
+train_dir="/home/norbert/Pulpit/dog vs cat/dataset/training_set"
 
+test_image = image.load_img(test_dir + '/dogs/dog.4128.jpg', target_size=(100,100))
 
-print(testing_image(test_dir + '/dogs/dog.4127.jpg'))
+test_image = image.img_to_array(test_image)
+
+res_list= ["Kotel","Pjesiula"]
+
+test_image = image.img_to_array(test_image)
+test_image = np.expand_dims(test_image, axis = 0)
+
+print(res_list[int(model.predict(test_image))])
+
+print(model.predict(test_image))
+
